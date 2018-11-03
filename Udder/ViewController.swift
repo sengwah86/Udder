f379210ee6bf64da3908a13c54c1bc4dd08c78eb
Seng Wah Pauh <pauhsengwah@gmail.com>
2018-11-03 17:55:11 +0800

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Seng Wah Pauh on 3/11/18.
+//  Copyright © 2018 Seng Wah Pauh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
